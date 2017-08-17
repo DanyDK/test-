866092d437db21a828797e8a7bffc36ceb86922a
Daniil <31095434+DanyDK@users.noreply.github.com>
2017-08-17 16:20:01 +0300

    Add files via upload

---
+++ JustFirst/JustFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 5; a < 10; a++)
+                Console.WriteLine($"Квадратный корень из {a} равен {a * a}");
+            Console.ReadKey();
+
+        }
+
+    }
+}
+        
+
+
